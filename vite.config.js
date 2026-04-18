a7da0377cc2a69cf34f68c1b6120c9d92c6d6afe
prateekumar27 <prateekumar27@gmail.com>
2026-04-18 21:49:45 +0530

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
   server: {
     proxy: {
       "/api": "http://localhost:3000",
